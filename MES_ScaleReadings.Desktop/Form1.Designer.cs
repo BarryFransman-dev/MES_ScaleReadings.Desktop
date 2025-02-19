571d4a264732aecebb3d12cdacfea39e568b14df
BarryFransman-dev <barry@nurturebrands.co.za>
2025-02-19 16:01:24 +0200

    Add project files.

---
+++ MES_ScaleReadings.Desktop/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MES_ScaleReadings.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet100 = new System.Windows.Forms.Button();
+            this.tbInfo100 = new System.Windows.Forms.TextBox();
+            this.btnDisConnect = new System.Windows.Forms.Button();
+            this.tbInfo101 = new System.Windows.Forms.TextBox();
+            this.btnGet101 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGet100
+            // 
+            this.btnGet100.Location = new System.Drawing.Point(381, 10);
+            this.btnGet100.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGet100.Name = "btnGet100";
+            this.btnGet100.Size = new System.Drawing.Size(114, 46);
+            this.btnGet100.TabIndex = 0;
+            this.btnGet100.Text = "Get 100";
+            this.btnGet100.UseVisualStyleBackColor = true;
+            this.btnGet100.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbInfo100
+            // 
+            this.tbInfo100.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tbInfo100.Location = new System.Drawing.Point(0, 0);
+            this.tbInfo100.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbInfo100.Multiline = true;
+            this.tbInfo100.Name = "tbInfo100";
+            this.tbInfo100.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInfo100.Size = new System.Drawing.Size(376, 378);
+            this.tbInfo100.TabIndex = 1;
+            // 
+            // btnDisConnect
+            // 
+            this.btnDisConnect.Location = new System.Drawing.Point(381, 326);
+            this.btnDisConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDisConnect.Name = "btnDisConnect";
+            this.btnDisConnect.Size = new System.Drawing.Size(103, 42);
+            this.btnDisConnect.TabIndex = 2;
+            this.btnDisConnect.Text = "Clear";
+            this.btnDisConnect.UseVisualStyleBackColor = true;
+            this.btnDisConnect.Click += new System.EventHandler(this.btnDisConnect_Click);
+            // 
+            // tbInfo101
+            // 
+            this.tbInfo101.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tbInfo101.Location = new System.Drawing.Point(500, 0);
+            this.tbInfo101.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbInfo101.Multiline = true;
+            this.tbInfo101.Name = "tbInfo101";
+            this.tbInfo101.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInfo101.Size = new System.Drawing.Size(376, 378);
+            this.tbInfo101.TabIndex = 3;
+            // 
+            // btnGet101
+            // 
+            this.btnGet101.Location = new System.Drawing.Point(381, 60);
+            this.btnGet101.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGet101.Name = "btnGet101";
+            this.btnGet101.Size = new System.Drawing.Size(114, 46);
+            this.btnGet101.TabIndex = 4;
+            this.btnGet101.Text = "Get 101";
+            this.btnGet101.UseVisualStyleBackColor = true;
+            this.btnGet101.Click += new System.EventHandler(this.btnGet101_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 198);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(409, 248);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(461, 274);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "PORT:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 378);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGet101);
+            this.Controls.Add(this.tbInfo101);
+            this.Controls.Add(this.btnDisConnect);
+            this.Controls.Add(this.tbInfo100);
+            this.Controls.Add(this.btnGet100);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet100;
+        private System.Windows.Forms.TextBox tbInfo100;
+        private System.Windows.Forms.Button btnDisConnect;
+        private System.Windows.Forms.TextBox tbInfo101;
+        private System.Windows.Forms.Button btnGet101;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MES_ScaleReadings.Desktop/Form1.cs
@@ -0,0 +1,264 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MES_ScaleReadings.Desktop
+{
+    public partial class Form1 : Form
+    {
+        //Server server = new Server();
+        public string Rx_Data;
+        public TcpClient tcpClient100;
+        public IAsyncResult asResult100;
+        public TcpClient tcpClient101;
+        public IAsyncResult asResult101;
+
+        public event EventHandler Received;
+        public Form1()
+        {
+            InitializeComponent();
+            //server.Received += new EventHandler(server_Received);
+        }
+
+        //public void server_Received(object sender, EventArgs e)
+        //{
+        //    //tbInfo.Text = server.Rx_Data;
+        //}
+
+        //public static void Connect(string server, int port)
+        //{
+        //    try
+        //    {
+        //        TcpClient client = new TcpClient(server, port);
+
+        //        var data = Encoding.ASCII.GetBytes("S");
+
+        //        var stream = client.GetStream();
+
+        //        data = new byte[257];
+
+        //        var responseData = string.Empty;
+
+        //        var bytes = stream.Read(data, 0, data.Length);
+        //        responseData = Encoding.ASCII.GetString(data, 0, bytes);
+
+        //        var msgStr = string.Format("Received: {0} ", responseData);
+        //        MessageBox.Show(msgStr);
+
+        //        stream.Close();
+        //        client.Close();
+        //    }
+        //    catch (ArgumentNullException e)
+        //    {
+        //        MessageBox.Show("ArgumentNullException: {0}" + e.Message);
+        //    }
+        //    catch (SocketException e)
+        //    {
+        //        MessageBox.Show("SocketException: {0}" + e.Message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //} // Connect
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetScaleReading("10.10.1.100", 0);
+            //tbInfo100.Text = string.Empty;
+            //tbInfo101.Text = string.Empty;
+
+
+            //tcpClient100 = new TcpClient("10.10.1.100", 1);
+            //asResult100 = HandleClientCommAsync100("10.10.1.100", 1);
+
+            //tcpClient101 = new TcpClient("10.10.1.101", 1);
+            //asResult101 = HandleClientCommAsync101("10.10.1.101", 1);
+        }
+
+        protected virtual void IsDataReceived(EventArgs e)
+        {
+            if (Received != null)
+                Received(this, e);
+        }
+
+        private async System.Threading.Tasks.Task HandleClientCommAsync100(string server, int port)
+        {
+            var clientStream100 = tcpClient100.GetStream();
+            var encoder = new ASCIIEncoding();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (tcpClient100.Connected)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    bytesRead = await clientStream100.ReadAsync(message, 0, 4096);
+                }
+                catch
+                {
+                    break;
+                }
+
+                var str = encoder.GetString(message, 0, bytesRead);
+                //if (decimal.Parse(Regex.Match(str.Substring(20, str.Length - 20).Trim(), @"\d+.+\d").Value) > 0)
+                //{
+                    tbInfo100.Text += "IP: " + tcpClient100.Client.RemoteEndPoint + "   Weight: " + Regex.Match(str.Substring(20, str.Length - 20).Trim(), @"-?\d+.+\d").Value + Environment.NewLine;
+                    tbInfo100.Refresh();
+                //}
+
+                IsDataReceived(EventArgs.Empty);
+            }
+        }
+
+        private async System.Threading.Tasks.Task HandleClientCommAsync101(string server, int port)
+        {
+            var clientStream101 = tcpClient101.GetStream();
+            var encoder1 = new ASCIIEncoding();
+
+            byte[] message1 = new byte[4096];
+            int bytesRead1;
+
+            while (tcpClient101.Connected)
+            {
+                bytesRead1 = 0;
+
+                try
+                {
+                    bytesRead1 = await clientStream101.ReadAsync(message1, 0, 4096);
+                }
+                catch
+                {
+                    break;
+                }
+
+                var str1 = encoder1.GetString(message1, 0, bytesRead1);
+                //if (decimal.Parse(Regex.Match(str1.Substring(20, str1.Length - 20).Trim(), @"\d+.+\d").Value) > 0)
+                //{
+                    tbInfo101.Text += "IP: " + tcpClient101.Client.RemoteEndPoint + "   Weight: " + Regex.Match(str1.Substring(20, str1.Length - 20).Trim(), @"-?\d+.+\d").Value + Environment.NewLine;
+                    tbInfo101.Refresh();
+                //}
+
+                IsDataReceived(EventArgs.Empty);
+            }
+        }
+
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+            tbInfo100.Text = string.Empty;
+            tbInfo101.Text = string.Empty;
+            //tcpClient100.Close();
+            //tcpClient101.Close();
+        }
+
+        private void GetScaleReading(string ip, int port)
+        {
+            TcpClient client = new TcpClient();
+            try
+            {
+
+                var PORT_NO = port;           //enter the scale IP port number
+                var SERVER_IP = ip;   //enter the scale IP address
+                                      //---data to send to the server---
+
+                //---create a TCPClient object at the IP and port no.---
+                client = new TcpClient(SERVER_IP, PORT_NO);
+                NetworkStream nwStream = client.GetStream();
+
+                byte[] ENQByteToSend = new byte[1];
+                ENQByteToSend[0] = 5; //put an ENQ (char(5)) into the byte array
+
+                //---send the ENQ---
+
+                //this.Controls.Find(string.Format("tbInfo10{0}", i), false).First().Text += "Sending : " + ENQByteToSend[0] + Environment.NewLine;
+
+                nwStream.Write(ENQByteToSend, 0, ENQByteToSend.Length);
+
+                //---read back the text--- //this could be the weight data
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                var str1 = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead) + Environment.NewLine; //"G 11-05-22 19:15:10      0.018 kg";//
+                var res = Regex.Match(str1.Substring(20, str1.Length - 20).Trim(), @"-?\d+.+\d").Value + Environment.NewLine;
+
+                //var lblUOM = "G";
+                //if (lblUOM == "G")
+                //{
+                //    res = (decimal.Parse(res) * 1000).ToString();
+                //}
+
+
+                this.Controls.Find(string.Format("tbInfo10{0}", 0), false).First().Text += "Received : IP: " + ip + " : " + res + Environment.NewLine;
+
+                //Console.ReadLine();
+                client.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                client.Close();
+            }
+        }
+
+        private void btnGet101_Click(object sender, EventArgs e)
+        {
+            GetScaleReading("10.10.1.101", 1);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            GetScaleReading(textBox1.Text, Convert.ToInt32(textBox2.Text));
+        }
+
+        //        IDictionary<Task<TcpClient>, Tuple<int, TcpListener>> tcpListeners = new Dictionary<Task<TcpClient>, Tuple<int, TcpListener>>();
+
+        //foreach (var port in Enumerable.Range(20000, 5))
+        //{
+        //    var tcpListener = new TcpListener(IPAddress.Any, port);
+
+        //        tcpListener.Start();
+
+        //    var task = tcpListener.AcceptTcpClientAsync();
+        //        var tcpListenerPortPair = new Tuple<int, TcpListener>(port, tcpListener);
+
+        //        tcpListeners.Add(task, tcpListenerPortPair);
+        //}
+
+        //    Task<TcpClient> tcpClientTask;
+
+        //while ((tcpClientTask = await Task.WhenAny(tcpListeners.Keys)) != null)
+        //{
+        //    var tcpListenerPortPair = tcpListeners[tcpClientTask];
+        //    var port = tcpListenerPortPair.Item1;
+        //    var tcpListener = tcpListenerPortPair.Item2;
+
+        //    tcpListeners.Remove(tcpClientTask);
+
+        //    // This needs to be async. What to do with its Task?
+        //    // It cannot be awaited here.
+        //    var handlerTask = HandleByPortNumber(tcpClientTask.Result, port);
+
+        //    var task = tcpListener.AcceptTcpClientAsync();
+
+        //    tcpListeners.Add(task, tcpListenerPortPair);
+        //}
+
+
+
+
+    }
+}
+
+
+
+
